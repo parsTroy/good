750d81ba3e17fdf1807004ee4ea2fe8f4b43c8c1
parsTroy <troyparsons@hotmail.com>
2025-07-15 14:34:33 -0400

    Revert

--- good/Models/Payment.cs
+++
@@ -1,9 +0,0 @@
-namespace good.Models;
-
-public class Payment
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public decimal Amount { get; set; }
-    public DateTime Date { get; set; } = DateTime.Now;
-    public string Type { get; set; } = "minimum"; // "minimum" or "extra"
-}
